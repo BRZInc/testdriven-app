31e5017c175a4ab44b5b0d0260dd3e17005e69bd
BRZInc <rustem.bedretdinov@gmail.com>
2020-05-08 00:53:27 +0300

    Added NavBar

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -3,15 +3,23 @@ import axios from 'axios';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
+import NavBar from './components/NavBar';
+import Form from './components/Form';
 import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             users: [],
             username: '',
-            email: ''
+            email: '',
+            title: 'TestDriven.io',
+            formData: {
+                username: '',
+                email: '',
+                password: ''
+            }
         };
 
         this.addUser = this.addUser.bind(this);
@@ -52,27 +60,44 @@ class App extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <br/>
-                        <Switch>
-                            <Route exact path='/' render={() => (
-                                <div>
-                                    <h1>All Users</h1>
-                                    <hr/><br/>
-                                    <AddUser
-                                        username={this.state.username}
-                                        email={this.state.email}
-                                        handleChange={this.handleChange}
-                                        addUser={this.addUser}
-                                    />
-                                    <br/>
-                                    <UsersList users={this.state.users}/>
-                                </div>
-                            )} />
-                            <Route exact path='/about' component={About} />
-                        </Switch>
+            <div>
+                <NavBar
+                    title={this.state.title}
+                />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <br/>
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1>All Users</h1>
+                                        <hr/><br/>
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            handleChange={this.handleChange}
+                                            addUser={this.addUser}
+                                        />
+                                        <br/>
+                                        <UsersList users={this.state.users}/>
+                                    </div>
+                                )} />
+                                <Route exact path='/about' component={About} />
+                                <Route exact path='/register' render={() => (
+                                  <Form
+                                    formType={'Register'}
+                                    formData={this.state.formData}
+                                  />
+                                )} />
+                                <Route exact path='/login' render={() => (
+                                  <Form
+                                    formType={'Login'}
+                                    formData={this.state.formData}
+                                  />
+                                )} />
+                            </Switch>
+                        </div>
                     </div>
                 </div>
             </div>
