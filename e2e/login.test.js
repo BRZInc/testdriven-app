876d9d4a431896da1a4a16923166c1a41cdd621a
BRZInc <rustem.bedretdinov@gmail.com>
2020-05-08 20:57:18 +0300

    Fixed failing e2e tests

--- e2e/login.test.js
+++ e2e/login.test.js
@@ -25,40 +25,6 @@ test(`should allow a user to sign in`, async (t) => {
 
   // log a user out
   await t
-    .click(Selector('a').withText('Log Out'))
-
-  // log a user in
-  await t
-    .navigateTo(`${TEST_URL}/login`)
-    .typeText('input[name="email"]', email)
-    .typeText('input[name="password"]', 'test')
-    .click(Selector('input[type="submit"]'));
-
-  // assert user is redirected to '/'
-  // assert '/' is displayed properly
-  const tableRow = Selector('td').withText(username).parent();
-
-  await t
-    .expect(Selector('H1').withText('All Users').exists).ok()
-    .expect(tableRow.child().withText(username).exists).ok()
-    .expect(tableRow.child().withText(email).exists).ok()
-    .expect(Selector('a').withText('User Status').exists).ok()
-    .expect(Selector('a').withText('Log Out').exists).ok()
-    .expect(Selector('a').withText('Register').exists).notOk()
-    .expect(Selector('a').withText('Log In').exists).notOk()
-});
-
-test(`should allow a user to sign in`, async (t) => {
-  // register user
-  await t
-    .navigateTo(`${TEST_URL}/register`)
-    .typeText('input[name="username"]', username)
-    .typeText('input[name="email"]', email)
-    .typeText('input[name="password"]', 'test')
-    .click(Selector('input[type="submit"]'));
-
-  // log a user out
-  await t
     .click(Selector('a').withText('Log Out'));
 
   // log a user in
